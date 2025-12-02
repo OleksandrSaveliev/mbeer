3492022be25bcd988b79a9973fbebe5b28f4ea57
Olexandr-Savelev <a.savelev.krm@gmail.com>
2025-12-02 16:31:23 +0200

    add data initialization

---
+++ src/main/java/com/tmdna/mbeer/config/DataInitializer.java
@@ -0,0 +1,89 @@
+package com.tmdna.mbeer.config;
+
+import com.tmdna.mbeer.model.Beer;
+import com.tmdna.mbeer.model.Customer;
+import com.tmdna.mbeer.repository.BeerRepository;
+import com.tmdna.mbeer.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        loadCustomerData();
+        loadBeerData();
+    }
+
+    private void loadBeerData() {
+        if (beerRepository.count() == 0) {
+            Beer beer1 = Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("Pale Ale")
+                    .upd("12356")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(122)
+                    .createdTime(LocalDateTime.now())
+                    .updatedTime(LocalDateTime.now())
+                    .build();
+
+            Beer beer2 = Beer.builder()
+                    .beerName("Crank")
+                    .beerStyle("IPA")
+                    .upd("12356222")
+                    .price(new BigDecimal("11.99"))
+                    .quantityOnHand(392)
+                    .createdTime(LocalDateTime.now())
+                    .updatedTime(LocalDateTime.now())
+                    .build();
+
+            Beer beer3 = Beer.builder()
+                    .beerName("Sunshine City")
+                    .beerStyle("IPA")
+                    .upd("12356")
+                    .price(new BigDecimal("13.99"))
+                    .quantityOnHand(144)
+                    .createdTime(LocalDateTime.now())
+                    .updatedTime(LocalDateTime.now())
+                    .build();
+
+            beerRepository.saveAll(Arrays.asList(beer1, beer2, beer3));
+        }
+    }
+
+    private void loadCustomerData() {
+        if (customerRepository.count() == 0) {
+            Customer customer1 = Customer.builder()
+                    .customerName("Customer 1")
+                    .createdTime(LocalDateTime.now())
+                    .updatedTime(LocalDateTime.now())
+                    .build();
+
+            Customer customer2 = Customer.builder()
+                    .customerName("Customer 2")
+                    .createdTime(LocalDateTime.now())
+                    .updatedTime(LocalDateTime.now())
+                    .build();
+
+            Customer customer3 = Customer.builder()
+                    .customerName("Customer 3")
+                    .createdTime(LocalDateTime.now())
+                    .updatedTime(LocalDateTime.now())
+                    .build();
+
+            customerRepository.saveAll(Arrays.asList(customer1, customer2, customer3));
+        }
+    }
+}
--- src/test/java/com/tmdna/mbeer/repository/BeerRepositoryTest.java
+++ src/test/java/com/tmdna/mbeer/repository/BeerRepositoryTest.java
@@ -1,17 +1,25 @@
 package com.tmdna.mbeer.repository;
 
+import com.tmdna.mbeer.dto.BeerDTO;
+import com.tmdna.mbeer.mapper.BeerMapper;
 import com.tmdna.mbeer.model.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 class BeerRepositoryTest {
+
     @Autowired
     BeerRepository beerRepository;
+
+    @Autowired
+    BeerMapper beerMapper;
 
     @Test
     void saveBeerTest() {
@@ -24,5 +32,16 @@ class BeerRepositoryTest {
         assertNotNull(savedBeer);
         assertNotNull(savedBeer.getId());
         assertEquals(beerName, savedBeer.getBeerName());
+
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(savedBeer);
+
+        assertNotNull(beerDTO);
+    }
+
+    @Test
+    void getAllBeersTest() {
+        List<Beer> beers = beerRepository.findAll();
+
+        assertEquals(3, beers.size());
     }
 }
