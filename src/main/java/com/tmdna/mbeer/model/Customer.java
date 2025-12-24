cd72a051f238908de247924d5a1f537d5172ed78
Olexandr-Savelev <a.savelev.krm@gmail.com>
2025-12-24 19:32:14 +0200

    fix sql schema

--- src/main/java/com/tmdna/mbeer/model/Customer.java
+++ src/main/java/com/tmdna/mbeer/model/Customer.java
@@ -2,7 +2,9 @@ package com.tmdna.mbeer.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -18,7 +20,8 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", nullable = false, updatable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", nullable = false, updatable = false)
     private UUID id;
     private String customerName;
 
